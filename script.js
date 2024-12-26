5a49ac323bcef4e4c07a9644690e9f12332dff75
ImShubham09 <singhashubham612@gmail.com>
2024-12-27 00:33:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+       function displayAlert() {
+            const firstName = document.getElementById('first_name').value;
+            const lastName = document.getElementById('last_name').value;
+            const phoneNumber = document.getElementById('phone_number').value;
+            const emailId = document.getElementById('email_id').value;
+
+            alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`);
+        }

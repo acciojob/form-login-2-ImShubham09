cf8b9fdc078427e9c666181c29a1fa3c4c38de05
ImShubham09 <singhashubham612@gmail.com>
2024-12-27 19:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
        function displayAlert() {
-            const firstName = document.getElementById('first_name').value;
-            const lastName = document.getElementById('last_name').value;
-            const phoneNumber = document.getElementById('phone_number').value;
-            const emailId = document.getElementById('email_id').value;
+            const firstName = document.getElementById('First Name').value;
+            const lastName = document.getElementById('Last Name').value;
+            const phoneNumber = document.getElementById('Phone Number').value;
+            const emailId = document.getElementById('Email ID').value;
 
-            alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`);
+            alert(`First Name: ${firstName} Last Name: ${lasId}`);tName} Phone Number: ${phoneNumber} Email ID: ${email
         }

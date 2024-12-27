26440a2ff232cf94c37f4a78c41847ebf15ecfb9
ImShubham09 <singhashubham612@gmail.com>
2024-12-27 19:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//your JS code here. If required.
-       function displayAlert() {
-            const firstName = document.getElementById('First Name').value;
-            const lastName = document.getElementById('Last Name').value;
-            const phoneNumber = document.getElementById('Phone Number').value;
-            const emailId = document.getElementById('Email ID').value;
+function displayAlert() {
+    const firstName = document.getElementById('First Name').value;
+    const lastName = document.getElementById('Last Name').value;
+    const phoneNumber = document.getElementById('Phone Number').value;
+    const emailId = document.getElementById('Email ID').value;
 
-            alert(`First Name: ${firstName} Last Name: ${lasId}`);tName} Phone Number: ${phoneNumber} Email ID: ${email
-        }
+    // Fixing the string interpolation
+    alert(`First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${emailId}`);
+}
